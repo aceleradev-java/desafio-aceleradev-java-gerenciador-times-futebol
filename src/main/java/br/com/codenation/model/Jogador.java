49189365b3c2d26b7367290d1dc5bab2eeee5cdb
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-24 09:58:05 -0300

    adiciona Model Jogador

---
+++ src/main/java/br/com/codenation/model/Jogador.java
@@ -0,0 +1,103 @@
+package br.com.codenation.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Jogador {
+    private Long id;
+    private Long idTime;
+    private String nome;
+    private LocalDate dataNascimento;
+    private Integer nivelHabilidade;
+    private BigDecimal salario;
+
+    public Jogador(Long id, Long idTime, String nome, LocalDate dataNascimento, Integer nivelHabilidade, BigDecimal salario) {
+        this.id = id;
+        this.idTime = idTime;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.nivelHabilidade = nivelHabilidade;
+        this.salario = salario;
+    }
+
+    public Jogador() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdTime() {
+        return idTime;
+    }
+
+    public void setIdTime(Long idTime) {
+        this.idTime = idTime;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Integer getNivelHabilidade() {
+        return nivelHabilidade;
+    }
+
+    public void setNivelHabilidade(Integer nivelHabilidade) {
+        this.nivelHabilidade = nivelHabilidade;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Jogador other = (Jogador) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" + "id=" + id + ", idTime=" + idTime + ", nome=" + nome + ", dataNascimento=" + dataNascimento + ", nivelHabilidade=" + nivelHabilidade + ", salario=" + salario + '}';
+    }
+}
