d830cc791830eae2ab1373e04c316a9008ff8a90
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-24 09:32:48 -0300

    adiciona model TimeDeFutebol

---
+++ src/main/java/br/com/codenation/model/TimeDeFutebol.java
@@ -0,0 +1,92 @@
+package br.com.codenation.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class TimeDeFutebol {
+    private Long id;
+    private String nome;
+    private LocalDate dataCriacao;
+    private String corUniformePrincipal;
+    private String corUniformeSecundario;
+
+    public TimeDeFutebol(Long id, String nome, LocalDate dataCriacao, String corUniformePrincipal, String corUniformeSecundario) {
+        this.id = id;
+        this.nome = nome;
+        this.dataCriacao = dataCriacao;
+        this.corUniformePrincipal = corUniformePrincipal;
+        this.corUniformeSecundario = corUniformeSecundario;
+    }
+
+    public TimeDeFutebol() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getCorUniformePrincipal() {
+        return corUniformePrincipal;
+    }
+
+    public void setCorUniformePrincipal(String corUniformePrincipal) {
+        this.corUniformePrincipal = corUniformePrincipal;
+    }
+
+    public String getCorUniformeSecundario() {
+        return corUniformeSecundario;
+    }
+
+    public void setCorUniformeSecundario(String corUniformeSecundario) {
+        this.corUniformeSecundario = corUniformeSecundario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TimeDeFutebol other = (TimeDeFutebol) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Time{" + "id=" + id + ", nome=" + nome + ", dataCriacao=" + dataCriacao + ", corUniformePrincipal=" + corUniformePrincipal + ", corUniformeSecundario=" + corUniformeSecundario + '}';
+    }
+}
